5295487654dac54256457f697cc3b9fb666a9cc7
mikaellindstrom <mikael.lindstrom@netlight.com>
2012-11-09 16:25:08 +0100

    * Added javadoc for SPiDConfiguration and SPiDConfigurationBuilder

--- SPiDSDK/src/com/schibsted/android/sdk/SPiDConfiguration.java
+++ SPiDSDK/src/com/schibsted/android/sdk/SPiDConfiguration.java
@@ -3,10 +3,7 @@ package com.schibsted.android.sdk;
 import android.content.Context;
 
 /**
- * Created with IntelliJ IDEA.
- * User: mikaellindstrom
- * Date: 10/8/12
- * Time: 4:33 PM
+ * Contains a configuration for the SPiD SDK
  */
 public class SPiDConfiguration {
 
@@ -24,7 +21,24 @@ public class SPiDConfiguration {
     private String apiVersion;
     private Context context;
 
-    public SPiDConfiguration(String clientID, String clientSecret, String appURLScheme, String serverURL, String redirectURL, String authorizationURL, String registrationURL, String lostPasswordURL, String tokenURL, String serverClientID, Boolean useMobileWeb, String apiVersion, Context context) {
+    /**
+     * Constructor for SPiDConfiguration object.
+     *
+     * @param clientID         SPiD client id
+     * @param clientSecret     SPiD client secret
+     * @param appURLScheme     Android app url scheme
+     * @param serverURL        SPiD server url
+     * @param redirectURL      SPiD redirect url
+     * @param authorizationURL SPiD authorization url
+     * @param registrationURL  SPiD registration url
+     * @param lostPasswordURL  SPiD lost password url
+     * @param tokenURL         SPiD token url
+     * @param serverClientID   SPiD client id for server
+     * @param useMobileWeb     Use mobile flag
+     * @param apiVersion       SPiD API version
+     * @param context          Android application context
+     */
+    protected SPiDConfiguration(String clientID, String clientSecret, String appURLScheme, String serverURL, String redirectURL, String authorizationURL, String registrationURL, String lostPasswordURL, String tokenURL, String serverClientID, Boolean useMobileWeb, String apiVersion, Context context) {
         this.clientID = clientID;
         this.clientSecret = clientSecret;
         this.appURLScheme = appURLScheme;
@@ -40,106 +54,184 @@ public class SPiDConfiguration {
         this.context = context;
     }
 
+    /**
+     * @return SPiD client id
+     */
     public String getClientID() {
         return clientID;
     }
 
+    /**
+     * @param clientID SPiD client id
+     */
     public void setClientID(String clientID) {
         this.clientID = clientID;
     }
 
+    /**
+     * @return SPiD client secret
+     */
     public String getClientSecret() {
         return clientSecret;
     }
 
+    /**
+     * @param clientSecret SPiD client secret
+     */
     public void setClientSecret(String clientSecret) {
         this.clientSecret = clientSecret;
     }
 
+    /**
+     * @return Android app url scheme
+     */
     public String getAppURLScheme() {
         return appURLScheme;
     }
 
+    /**
+     * @param appURLScheme Android app url scheme
+     */
     public void setAppURLScheme(String appURLScheme) {
         this.appURLScheme = appURLScheme;
     }
 
+    /**
+     * @return SPiD server url
+     */
     public String getServerURL() {
         return serverURL;
     }
 
+    /**
+     * @param serverURL SPiD server url
+     */
     public void setServerURL(String serverURL) {
         this.serverURL = serverURL;
     }
 
+    /**
+     * @return SPiD redirect url, default value: <code>getAppURLScheme() + "://";</code>
+     */
     public String getRedirectURL() {
         return redirectURL;
     }
 
+    /**
+     * @param redirectURL SPiD redirect url
+     */
     public void setRedirectURL(String redirectURL) {
         this.redirectURL = redirectURL;
     }
 
+    /**
+     * @return SPiD authorization url, default value: <code>getServerURL() + "/auth/login";</code>
+     */
     public String getAuthorizationURL() {
         return authorizationURL;
     }
 
+    /**
+     * @param authorizationURL SPiD authorization url
+     */
     public void setAuthorizationURL(String authorizationURL) {
         this.authorizationURL = authorizationURL;
     }
 
+    /**
+     * @return SPiD registration url, default value: <code>getServerURL() + "/auth/signup";</code>
+     */
     public String getRegistrationURL() {
         return registrationURL;
     }
 
+    /**
+     * @param registrationURL SPiD registration url
+     */
     public void setRegistrationURL(String registrationURL) {
         this.registrationURL = registrationURL;
     }
 
+    /**
+     * @return SPiD lost password url, default value: <code>getServerURL() + "/auth/forgotpassword";</code>
+     */
     public String getLostPasswordURL() {
         return lostPasswordURL;
     }
 
+    /**
+     * @param lostPasswordURL SPiD lost password url
+     */
     public void setLostPasswordURL(String lostPasswordURL) {
         this.lostPasswordURL = lostPasswordURL;
     }
 
+    /**
+     * @return SPiD token url, default value: <code>getServerURL() + "/oauth/token";</code>
+     */
     public String getTokenURL() {
         return tokenURL;
     }
 
+    /**
+     * @param tokenURL SPiD token url
+     */
     public void setTokenURL(String tokenURL) {
         this.tokenURL = tokenURL;
     }
 
+    /**
+     * @return SPiD API version, default 2
+     */
     public String getApiVersion() {
         return apiVersion;
     }
 
+    /**
+     * @param apiVersion SPiD API version
+     */
     public void setApiVersion(String apiVersion) {
         this.apiVersion = apiVersion;
     }
 
+    /**
+     * @return SPiD client id for server, used for one time code. Default value: <code>getClientId()</code>
+     */
     public String getServerClientID() {
         return serverClientID;
     }
 
+    /**
+     * @param serverClientID SPiD client id for server
+     */
     public void setServerClientID(String serverClientID) {
         this.serverClientID = serverClientID;
     }
 
+    /**
+     * @return If use mobile flag should be set, default value: <code>true</code>
+     */
     public Boolean getUseMobileWeb() {
         return useMobileWeb;
     }
 
+    /**
+     * @param useMobileWeb Use mobile flag
+     */
     public void setUseMobileWeb(Boolean useMobileWeb) {
         this.useMobileWeb = useMobileWeb;
     }
 
+    /**
+     * @return Android application context
+     */
     public Context getContext() {
         return context;
     }
 
+    /**
+     * @param context Android application context
+     */
     public void setContext(Context context) {
         this.context = context;
     }
--- SPiDSDK/src/com/schibsted/android/sdk/SPiDConfigurationBuilder.java
+++ SPiDSDK/src/com/schibsted/android/sdk/SPiDConfigurationBuilder.java
@@ -3,13 +3,9 @@ package com.schibsted.android.sdk;
 import android.content.Context;
 
 /**
- * Created with IntelliJ IDEA.
- * User: mikaellindstrom
- * Date: 10/8/12
- * Time: 4:33 PM
+ * Builder class for SPiDConfiguration
  */
 public class SPiDConfigurationBuilder {
-
     private String clientID;
     private String clientSecret;
     private String appURLScheme;
@@ -24,81 +20,145 @@ public class SPiDConfigurationBuilder {
     private String apiVersion = "2";
     private Context context;
 
-    public SPiDConfigurationBuilder() {
-    }
-
+    /**
+     * @param clientID SPiD client id
+     * @return The SPiDConfigurationBuilder
+     */
     public SPiDConfigurationBuilder clientID(String clientID) {
         this.clientID = clientID;
         return this;
     }
 
+    /**
+     * @param clientSecret SPiD client secret
+     * @return The SPiDConfigurationBuilder
+     */
     public SPiDConfigurationBuilder clientSecret(String clientSecret) {
         this.clientSecret = clientSecret;
         return this;
     }
 
+    /**
+     * @param appURLScheme Android app url scheme
+     * @return The SPiDConfigurationBuilder
+     */
     public SPiDConfigurationBuilder appURLScheme(String appURLScheme) {
         this.appURLScheme = appURLScheme;
         return this;
     }
 
+    /**
+     * @param serverURL SPiD server url
+     * @return The SPiDConfigurationBuilder
+     */
     public SPiDConfigurationBuilder serverURL(String serverURL) {
         this.serverURL = serverURL;
         return this;
     }
 
+    /**
+     * @param redirectURL SPiD redirect url
+     * @return The SPiDConfigurationBuilder
+     */
     public SPiDConfigurationBuilder redirectURL(String redirectURL) {
         this.redirectURL = redirectURL;
         return this;
     }
 
+    /**
+     * @param authorizationURL SPiD authorization url
+     * @return The SPiDConfigurationBuilder
+     */
     public SPiDConfigurationBuilder authorizationURL(String authorizationURL) {
         this.authorizationURL = authorizationURL;
         return this;
     }
 
+    /**
+     * @param registrationURL SPiD registration url
+     * @return The SPiDConfigurationBuilder
+     */
     public SPiDConfigurationBuilder registrationURL(String registrationURL) {
         this.registrationURL = registrationURL;
         return this;
     }
 
+    /**
+     * @param lostPasswordURL SPiD lost password url
+     * @return The SPiDConfigurationBuilder
+     */
     public SPiDConfigurationBuilder lostPasswordURL(String lostPasswordURL) {
         this.lostPasswordURL = lostPasswordURL;
         return this;
     }
 
+    /**
+     * @param tokenURL SPiD token url
+     * @return The SPiDConfigurationBuilder
+     */
     public SPiDConfigurationBuilder tokenURL(String tokenURL) {
         this.tokenURL = tokenURL;
         return this;
     }
 
+    /**
+     * @param serverClientID SPiD client id for server
+     * @return The SPiDConfigurationBuilder
+     */
     public SPiDConfigurationBuilder serverClientID(String serverClientID) {
         this.serverClientID = serverClientID;
         return this;
     }
 
+    /**
+     * @param apiVersion SPiD API version
+     * @return The SPiDConfigurationBuilder
+     */
     public SPiDConfigurationBuilder apiVersion(String apiVersion) {
         this.apiVersion = apiVersion;
         return this;
     }
 
+    /**
+     * @param context Android application context
+     * @return The SPiDConfigurationBuilder
+     */
     public SPiDConfigurationBuilder context(Context context) {
         this.context = context;
         return this;
     }
 
+    /**
+     * Checks that supplied string is not empty, otherwise throws exception
+     *
+     * @param string
+     * @param errorMessage
+     * @throws IllegalArgumentException
+     */
     public void isEmptyString(String string, String errorMessage) {
         if (string == null || string.trim().equals("")) {
             throw new IllegalArgumentException(errorMessage);
         }
     }
 
+    /**
+     * Checks if supplied object is not null, otherwise throws exception
+     *
+     * @param object
+     * @param errorMessage
+     * @throws IllegalArgumentException
+     */
     public void isNull(Object object, String errorMessage) {
         if (object == null) {
             throw new IllegalArgumentException(errorMessage);
         }
     }
 
+    /**
+     * Builds a SPiDConfiguration object from the supplied values. It also check all that all mandatory values are set and generates default values for non-mandatory values that are missing.
+     *
+     * @return A SPiDConfiguration object
+     */
     public SPiDConfiguration build() {
         isEmptyString(clientID, "ClientID is missing");
         isEmptyString(clientSecret, "ClientSecret is missing");
