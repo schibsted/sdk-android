03eb2463db432c61ee364b35dff9081859808273
mikaellindstrom <mikael.lindstrom@gmail.com>
2013-04-03 00:07:12 +0200

    Changed testrunner for tests

--- SPiDSDKTests/src/test/com/schibsted/android/sdk/SPiDAccessTokenTest.java
+++ SPiDSDKTests/src/test/com/schibsted/android/sdk/SPiDAccessTokenTest.java
@@ -2,6 +2,8 @@ package com.schibsted.android.sdk;
 
 import com.schibsted.android.sdk.accesstoken.SPiDAccessToken;
 import com.schibsted.android.sdk.exceptions.SPiDAccessTokenException;
+import com.xtremelabs.robolectric.RobolectricTestRunner;
+import com.xtremelabs.robolectric.bytecode.RobolectricClassLoader;
 import org.json.JSONObject;
 import org.junit.Before;
 import org.junit.runner.RunWith;
@@ -14,7 +16,7 @@ import static junit.framework.Assert.assertNull;
 import static junit.framework.TestCase.assertEquals;
 import static junit.framework.TestCase.assertTrue;
 
-@RunWith(SPiDTestRunner.class)
+@RunWith(RobolectricTestRunner.class)
 public class SPiDAccessTokenTest {
 
     private static String TestAccessToken = "test-access-token";
